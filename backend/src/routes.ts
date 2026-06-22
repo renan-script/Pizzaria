fc72afdbccc107970f68b7ba5a6bb66cc9a0e88d
renan <renan.rodrigues@produzindocerto.com.br>
2026-06-22 10:36:49 -0300

    Separando a arquitetura do projeto

---
+++ backend/src/routes.ts
@@ -0,0 +1,11 @@
+import { Router, Response, Request } from 'express'
+import { CreateUserController } from './controllers/user/CreateUserController'
+
+const router = Router();
+
+router.post("/users", new CreateUserController().handle);
+
+export { router };
+
+
+
---
+++ backend/src/services/user/CreateUserService.ts
@@ -0,0 +1,10 @@
+class CreateUserService {
+    async execute() {
+        console.log("Executando Serviço");
+
+        return "Usuário criado!";
+
+    }
+}
+
+export { CreateUserService };
